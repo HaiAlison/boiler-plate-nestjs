e5099d1ff7b7b023e36f97f600d47d86fcdece91
haialison <hai.mai@unibiz.io>
2023-08-18 09:52:36 +0700

    feature/mongodb-and-dockerfile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:16-alpine
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install --legacy-peer-deps
+
+COPY . .
+
+RUN npm run build
+
+EXPOSE 3000
+
+CMD ["npm", "run", "start:prod"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,27 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from './utils/config/database/config.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: ['.env'], isGlobal: true }),
     ConfigModule,
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_NAME}
+      ?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000`,
+      {
+        connectionFactory: (connection) => {
+          console.log('Starting MongoDB connection...'); // Log at the start
+          connection.on('connected', () => {
+            console.log('Connected to MongoDB');
+          });
+          connection._events.connected();
+          return connection;
+        },
+      },
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
