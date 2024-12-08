27f4958ab010f7c986c16a074b219c45e26f473f
haialison <hai.mai@unibiz.io>
2024-12-09 00:12:14 +0700

    feature/i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,22 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from './utils/config/database/config.service';
+import { join } from 'path';
+import { HeaderResolver, I18nModule } from 'nestjs-i18n';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: ['.env'], isGlobal: true }),
     ConfigModule,
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    I18nModule.forRoot({
+      fallbackLanguage: process.env.DEFAULT_LANGUAGE,
+      loaderOptions: {
+        path: join(__dirname, 'utils/i18n/'),
+        watch: true,
+      },
+      resolvers: [new HeaderResolver(['x-language'])],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
