974b5eab438398b3126fc67bb29fce242a4444da
haialison <hai.mai@unibiz.io>
2025-05-07 01:35:17 +0700

    add generate_code sequence

--- src/migrations/1746555582433-generate_code.ts
+++ src/migrations/1746555582433-generate_code.ts
@@ -6,7 +6,7 @@ export class GenerateCode1746555582433 implements MigrationInterface {
     
 CREATE TABLE IF NOT EXISTS public.code_sequence
 (
-    id serial Primary key,
+    id serial NOT NULL,
     created_at timestamp with time zone NOT NULL DEFAULT now(),
     updated_at timestamp with time zone DEFAULT now(),
     deleted_at timestamp with time zone,
