f5c65b2dcfda06ed80532eb98619bb0a33a81d9f
haialison <hai.mai@unibiz.io>
2025-05-07 22:40:49 +0700

    feature/send-gmail: update Google OAuth guard and strategy to handle errors and adjust scopes

--- src/auth/guards/google-oauth.guard.ts
+++ src/auth/guards/google-oauth.guard.ts
@@ -4,9 +4,24 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 @Injectable()
 export class GoogleOauthGuard extends AuthGuard('google') {
   constructor() {
-    super({ accessType: 'offline' });
+    super({
+      accessType: 'offline',
+      prompt: 'consent',
+      scope: ['email', 'profile', 'https://mail.google.com/'],
+    });
   }
   canActivate(context: ExecutionContext) {
+    if (context.switchToHttp().getRequest().query['error']) {
+      console.error(
+        'Google OAuth Error:',
+        context.switchToHttp().getRequest().query['error'],
+      );
+      //redirect to frontend with error
+      context
+        .switchToHttp()
+        .getResponse()
+        .redirect(`${process.env.FRONTEND_URL}/auth/redirect?error=1`);
+    }
     return super.canActivate(context);
   }
 }
